3fcf486d84ca7f075685ca4d866e8f3213b94727
jsween5723 <gusckd57@naver.com>
2025-06-22 03:02:00 +0900

    build: init project

---
+++ src/main/kotlin/io/sween/largescalesystem/LargeScaleSystemApplication.kt
@@ -0,0 +1,11 @@
+package io.sween.largescalesystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LargeScaleSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<LargeScaleSystemApplication>(*args)
+}

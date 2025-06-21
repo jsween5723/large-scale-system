c525956631465f126c52e3b7885ff653fb1c7283
jsween5723 <jsween5723@gmail.com>
2025-06-22 03:02:00 +0900

    build: init project

---
+++ src/test/kotlin/io/sween/largescalesystem/LargeScaleSystemApplicationTests.kt
@@ -0,0 +1,12 @@
+package io.sween.largescalesystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LargeScaleSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
